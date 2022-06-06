62a6dd109877001e1741484276d1c32479c46368
Amin Ahmady <aminahmadydeveloper@gmail.com>
2022-06-07 02:06:24 +0430

    Added Pun Engine

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 
 function indexGet(request: Request, response: Response) {
-  response.status(200).send("Hello World!");
+  response.status(200).render("index", {
+    title: "Index",
+    message: "Test Express App With PUG Engine",
+  });
 }
 
 export { indexGet };
--- src/expressApp.ts
+++ src/expressApp.ts
@@ -4,6 +4,9 @@ import indexRouter from "@/routes/indexRouter";
 
 const expressApp = express();
 
+expressApp.set("views", __dirname + "/views");
+expressApp.use(express.static(__dirname + "/views"));
+expressApp.set("view engine", "pug");
 expressApp.use(morgan("dev"));
 expressApp.use("/", indexRouter);
 
